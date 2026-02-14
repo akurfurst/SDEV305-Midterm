6065584bf3821d5b5ba66f9f99ea468c45d312aa
akurfurst <akurfurst01@gamil.com>
2026-02-13 19:41:08 -0800

    Added client-side validation

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+document.getElementById("booking-form").onsubmit = () =>{
+
+
+
+    let isValid = true;
+    clearErrors();
+
+    let destination = document.getElementById("destination").value;
+    if(destination == "none"){
+        document.getElementById("err-destination").style.visibility = "visible";
+        isValid = false;
+    }
+
+    let date = document.getElementById("date").value;
+    if(!date){
+        document.getElementById("err-date").style.visibility = "visible";
+        isValid = false
+    }
+
+    let travelers = document.getElementById("travelers").value;
+    if(travelers < 1){
+        document.getElementById("err-travelers").style.visibility = "visible"
+    }
+
+    return isValid;
+}
+
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err");
+    for (let i = 0; i < errors.length; i++) {
+        errors[i].style.visibility = "hidden"
+    }
+
+}
